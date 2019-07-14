f42a572d850e8d61b6d4ed2eac04fa2b4e110e80
arnaud <arnaud.sinays@epitech.eu>
2019-07-14 19:13:01 +0200

    feat: init script

---
+++ init_conf.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+set -e
+
+BIRI_REPO="https://github.com/DeJonge00/PythonBot.git"
+REPO_BRANCH="api"
+BIRI_FOLDER="biri"
+
+setting_up () {
+    cd biribiri
+    curl "https://dl.dafont.com/dl/?f=ringbearer" --output ringbearer.zip
+    unzip ringbearer.zip -d ringbearer
+}
+
+download_sources () {
+    git clone ${BIRI_REPO}
+    cd PythonBot
+    git checkout ${REPO_BRANCH}
+}
+
+check_stuff () {
+    [ ! -f "requirements.txt" ] && echo "requirements.txt not found, you need this file in order to start the Bot."
+    echo "Don't forget the secrets as told in the README file"
+}
+
+setting_up
+download_sources
+check_stuff
